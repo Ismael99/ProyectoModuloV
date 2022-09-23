0a2e571216a505051d4b9d84d58678784b1fc364
Sergio Bernal <sbernal@applaudostudios.com>
2022-09-23 12:55:35 -0600

    ♻️ accept multiple roles

--- app/Filters/RoleFilter.php
+++ app/Filters/RoleFilter.php
@@ -28,7 +28,7 @@ class RoleFilter implements FilterInterface
      */
     public function before(RequestInterface $request, $arguments = null)
     {
-        $role = $arguments[0];
+        $role = $arguments;
         $model = new UsuarioModel();
         $user = $model
             ->where('usuario.usuario_username', $request->user->usuario_username)
@@ -37,7 +37,7 @@ class RoleFilter implements FilterInterface
 
         $request->user = $user;
 
-        if ($role !== null && $role !== $user->rol_nombre && $user->rol_nombre !== 'Admin') {
+        if ($role !== null && !in_array($user->rol_nombre, $role) && $user->rol_nombre !== 'Admin') {
             $response = service('response');
             $response->setBody('Unauthorized');
             $response->setStatusCode(403);
